30dfe20f0e0b7112a37bca0d0d5513c685f2e9c0
Dylan Plecki <dylanplecki@gmail.com>
2014-04-16 15:32:44 -0400

    Revert "Update versioning and remove CBA dependency"
    This reverts commit 69d3bbbd29ac990a1421eeec0291c0ff601cbab2.

--- @sqfrcon/addons/sqfrcon/config.cpp
+++ @sqfrcon/addons/sqfrcon/config.cpp
@@ -4,7 +4,7 @@ class sqfrcon
 	/* General Information */
 	name = "SQF RCON";
 	dir = "@sqfrcon";
-	version = "1.0.1.4";
+	version = "1.0.1.3";
 	author[] = {"Naught"};
 	authorUrl = "http://bitbucket.org/dylanplecki/sqf-rcon"; // URL
 	
@@ -35,17 +35,6 @@ class CfgMods {
 	class sqfrcon_mod : sqfrcon {};
 };
 
-class CfgFunctions
-{
-	class sqfrcon
-	{
-		class core
-		{
-			class server_handler
-			{
-				preInit = 1;
-				file = "\x\naught\addons\sqfrcon\server_handler.fsm";
-			};
-		};
-	};
+class Extended_PreInit_EventHandlers {
+	sqfrcon = "[] execFSM '\x\naught\addons\sqfrcon\server_handler.fsm'";
 };
